15cd3d84d4659aa8707d7a8c6a1b504d0bee42f7
JC Franco <jfranco@esri.com>
2015-07-22 13:21:54 -0700

    Simplify mixedInMessages module.

--- modular-oo-demo/mixedInMessages.js
+++ modular-oo-demo/mixedInMessages.js
@@ -1,21 +1,17 @@
 define([
   "./messages",
 
-  "dojo/_base/lang",
-
   "dojo/string"
 ],
 function(
   messages,
-  lang,
   string
 ) {
 
-  var mixedInMessages = {
-    greetings: string.substitute("${0} ${1}", [messages.hello, messages.uc2015])
-  };
-
   // expose module definition (object)
-  return lang.mixin(mixedInMessages, messages);
+  return {
+    greetings: string.substitute("${0} ${1}", [messages.hello, messages.uc2015]),
+    messages: messages
+  };
 
 });
